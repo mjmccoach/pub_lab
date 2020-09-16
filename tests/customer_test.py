22225d0a4e1ed7ef7f97822b80fdcb9c9fee1af0
Mathew-Tomaney <mat.tomaney@gmail.com>
2020-09-16 13:17:03 +0100

    added drink_test
    all classes setp-up and test succesfully

--- tests/customer_test.py
+++ tests/customer_test.py
@@ -5,8 +5,8 @@ class TestCustomer(unittest.TestCase):
     def setUp(self):
         self.customer = Customer("Harrison", 30.00)
 
-    def customer_has_name(self):
+    def test_customer_has_name(self):
         self.assertEqual("Harrison", self.customer.name)
 
-    def customer_has_wallet(self):
+    def test_customer_has_wallet(self):
         self.assertEqual(30.00, self.customer.wallet)
--- tests/drink_test.py
+++ tests/drink_test.py
@@ -4,3 +4,9 @@ from src.drink import Drink
 class TestDrink(unittest.TestCase):
     def setUp(self):
         self.drink = Drink("Rum", 5.50)
+
+    def test_drink_has_name(self):
+        self.assertEqual("Rum", self.drink.name)
+
+    def test_drink_has_price(self):
+        self.assertEqual(5.50, self.drink.price)
